4f525b3fe356a6892e21ec6f10600311ac51040e
kyliehefner <kyliehefner@verizon.net>
2022-05-17 18:28:39 -0700

    Queried retirement info

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,10 +1,14 @@
 -- Creating tables for PH-EmployeeDB
+DROP TABLE IF EXISTS departments;
+
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+
+DROP TABLE IF EXISTS employees;
 
 CREATE TABLE employees (
      emp_no INT NOT NULL,
@@ -16,6 +20,8 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 
+DROP TABLE IF EXISTS dept_manager;
+
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -26,6 +32,8 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+DROP TABLE IF EXISTS salaries;
+
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
@@ -35,15 +43,18 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
+DROP TABLE IF EXISTS dept_emp;
+
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
+
+DROP TABLE IF EXISTS titles;
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
@@ -51,6 +62,5 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
