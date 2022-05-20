76b38ce5d5a6322aa91764a52a312943d26839e4
kyliehefner <kyliehefner@verizon.net>
2022-05-19 22:09:25 -0700

    Employees eligible for mentorship program

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -12,6 +12,7 @@ FROM employees as e
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
+-- Only most recent titles of retiring employees
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no)
     emp_no,
@@ -29,3 +30,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- Employees eligible for mentorship program
+SELECT DISTINCT ON (emp_no)
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    dp.from_date,
+    dp.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as e
+    INNER JOIN dept_emp as dp
+        ON (e.emp_no = dp.emp_no)
+    LEFT JOIN titles as t
+        ON (e.emp_no = t.emp_no)
+WHERE (dp.to_date = '9999-01-01')
+    AND (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
